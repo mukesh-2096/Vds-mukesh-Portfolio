ce0e4a2973c056b7c8a2d9eed57ce8310ba52e2a
mukesh-2096 <durgasaimukeshvantakula5764@gmail.com>
2026-02-18 17:56:25 +0530

    added splash screen to prevent loading issues

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,23 @@ import { Projects } from "@/components/projects";
 import { Achievements } from "@/components/achievements";
 import { Contact } from "@/components/contact";
 import { Footer } from "@/components/footer";
+import { PageContent } from "@/components/page-content";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Achievements />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <PageContent>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Achievements />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </PageContent>
   );
 }
